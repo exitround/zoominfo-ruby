7a263100dab96f4d46f84ef21d820e5c1f1bb182
Greg Dean <dean.greg@gmail.com>
2016-11-08 11:18:40 -0800

    person match API

--- lib/zoominfo-ruby/person.rb
+++ lib/zoominfo-ruby/person.rb
@@ -1,12 +1,19 @@
 module ZoomInfo
   class Person < ZoomInfo::Base
     SEARCH_NON_KEY_PARAMS = %w(pc outputType outputFieldOptions key rpp page SortBy SortOrder)
+    MATCH_NON_KEY_PARAMS = %w(pc key numMatches echoInput outputFieldOptions matchCompany uniqueInputId)
     DETAIL_KEY_PARAMS = %w(PersonID EmailAddress)
 
     def search(query = {})
       key_values = query.reject{|k, v| SEARCH_NON_KEY_PARAMS.include?(k.to_s)}.values
       query[:key] = generate_key(key_values, @api_key)
       self.class.get("/person/search", query: query).parsed_response
+    end
+
+    def match(query = {})
+      key_values = query.reject{|k, v| MATCH_NON_KEY_PARAMS.include?(k.to_s)}.values
+      query[:key] = generate_key(key_values, @api_key)
+      self.class.get("/person/match", query: query).parsed_response
     end
 
     def detail(query = {})
